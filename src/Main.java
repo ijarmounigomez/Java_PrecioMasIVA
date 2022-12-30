7c8d9710adb257b94bad2a67a3d598b07750f457
Ismael Jarmouni Gomez <ijarmounigomez@gmail.com>
2022-12-30 17:44:05 +0100

    Added functionality to read user input by the use of the Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static double precioMasIVA() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number and press enter: ");
+
+        double enteredNumber = scanner.nextDouble();
+        double iva = enteredNumber * (0.21);
+        double precioFinal = enteredNumber + iva;
+
+
+        return precioFinal;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(precioMasIVA());
     }
 }
